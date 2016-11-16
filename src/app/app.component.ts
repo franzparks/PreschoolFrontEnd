29a5d265f86465c268ef4068c82b5b8a10006e4f
franzparks <franzparks@gmail.com>
2016-11-16 14:38:05 -0800

    correct dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ const NAVITEMS : NavItem[] = [
 	},
 	{
 		navTitle  : 'Main Title3',
-		navSubTitle  : 'Sub Title2',
-		//navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		navSubTitle  : 'Sub Title3',
+		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
 		//navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
         //navLinksCol3 : [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
 
