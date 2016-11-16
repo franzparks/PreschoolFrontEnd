06909426d6b6f838479908cd364ff579ab9dd3f7
franzparks <franzparks@gmail.com>
2016-11-16 03:48:44 -0800

    use ternary op for conditional styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,18 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
+
   title = 'Welcome To Preschool Rater';
-  navStyle = 'b-dropdown-grid-container';
+  navLinksCol1 : any[];
+  navLinksCol2 : any[];
+  navLinksCol3 : any[];
+
+  constructor(){}
+
+  navStyle = (this.navLinksCol2 !== [] ? 'b-dropdown-grid-container b-dropdown-grid-container-three-column' : 'b-dropdown-grid-container ' );
   navTitle : string = 'Main Title';
   navSubTitle : string = 'Sub Title';
-  navLinksCol1 : any[] = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
-  //navLinksCol2 : any[] = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
-  //navLinksCol3 : any[] = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
+  this.navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
+  this.navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
+  this.navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
 }
