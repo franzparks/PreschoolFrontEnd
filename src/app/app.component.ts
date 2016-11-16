bbe813457ec01cbea79b5032f11b879d05a2e042
franzparks <franzparks@gmail.com>
2016-11-16 11:21:48 -0800

    use ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Welcome To Preschool Rater';
+  navTitle : string;
+  navSubTitle : string;
   navLinksCol1 : any[];
   navLinksCol2 : any[];
   navLinksCol3 : any[];
 
-  constructor(){}
+  constructor(){
+    this.navTitle = '';
+    this.navSubTitle = '';
+  	this.navLinksCol1 = [];
+  	this.navLinksCol2 = [];
+  	this.navLinksCol3 = [];
+  }
 
   navStyle = (this.navLinksCol2 !== [] ? 'b-dropdown-grid-container b-dropdown-grid-container-three-column' : 'b-dropdown-grid-container ' );
-  navTitle : string = 'Main Title';
-  navSubTitle : string = 'Sub Title';
-  this.navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
-  this.navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
-  this.navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
+  //this.navTitle  = 'Main Title';
+  //this.navSubTitle  = 'Sub Title';
+  //this.navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
+  //this.navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
+  //this.navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
 }
