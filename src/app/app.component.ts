86c8acedb435cc95a0e0512771766f68210a7baa
franzparks <franzparks@gmail.com>
2016-11-15 22:19:11 -0800

    import nav-item-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {NavItemComponentComponent} from './nav-item-component/nav-item-component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'Welcome To Preschool Rater';
