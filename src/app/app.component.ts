3c14c39f313a4e93fb3e90a5554b7a6a4c41e359
franzparks <franzparks@gmail.com>
2016-11-16 14:04:52 -0800

    add dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,36 @@ import {NavItem} from './nav-item';
   styleUrls: ['./app.component.css'],
 
 })
+
+export const NAV-ITEMS = [
+
+	{
+		navTitle  = 'Main Title1',
+		navSubTitle  = 'Sub Title1',
+		navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+        navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+
+	},
+	{
+		navTitle  = 'Main Title2',
+		navSubTitle  = 'Sub Title2',
+		navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+        navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+
+	}
+];
 export class AppComponent {
 
   title = 'Welcome To Preschool Rater';
-  navTitle : string = 'One';
-  navSubTitle : string = 'Two';
-  navLinksCol1 : any[];
-  navLinksCol2 : any[];
-  navLinksCol3 : any[];
+  navItems : NavItem[] = [];
 
   constructor(){
-    this.navTitle = 'Hey';
-    this.navSubTitle = 'HEllo';
-  	this.navLinksCol1 = [];
-  	this.navLinksCol2 = [];
-  	this.navLinksCol3 = [];
+    this.navItems = NAV-ITEMS;
   }
 
-  navStyle = (this.navLinksCol2 !== [] ? 'b-dropdown-grid-container b-dropdown-grid-container-three-column' : 'b-dropdown-grid-container ' );
-  //this.navTitle  = 'Main Title';
-  //this.navSubTitle  = 'Sub Title';
-  //this.navLinksCol1  = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
-  //this.navLinksCol2 = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
-  //this.navLinksCol3 = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
+  //navStyle = (this.navLinksCol2 !== [] ? 'b-dropdown-grid-container b-dropdown-grid-container-three-column' : //'b-dropdown-grid-container ' );
+
+  
 }
