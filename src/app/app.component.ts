f07e910e440ea4e8ab611d3e8c5a4e628d542488
franzparks <franzparks@gmail.com>
2016-11-16 01:02:01 -0800

    move nav variables to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavItemComponentComponent} from './nav-item-component/nav-item-component';
+//import {NavItemComponentComponent} from './nav-item-component/nav-item-component';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,10 @@ import {NavItemComponentComponent} from './nav-item-component/nav-item-component
 })
 export class AppComponent {
   title = 'Welcome To Preschool Rater';
+
+  navTitle : string = 'Main Title';
+  navSubTitle : string = 'Sub Title';
+  navLinksCol1 : any[] = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
+  //navLinksCol2 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
+  //navLinksCol3 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
 }
--- src/app/nav-item-component/nav-item-component.component.ts
+++ src/app/nav-item-component/nav-item-component.component.ts
@@ -14,6 +14,6 @@ export class NavItemComponentComponent implements OnInit {
   navTitle : string = 'Main Title';
   navSubTitle : string = 'Sub Title';
   navLinksCol1 : any[] = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
-  navLinksCol2 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
-  navLinksCol3 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
+  //navLinksCol2 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
+  //navLinksCol3 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
 }
