fed142f7a2f9d52ab2785f788894adf40c83d2e8
franzparks <franzparks@gmail.com>
2016-11-16 03:27:01 -0800

    pass style class from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome To Preschool Rater';
-
+  navStyle = 'b-dropdown-grid-container';
   navTitle : string = 'Main Title';
   navSubTitle : string = 'Sub Title';
   navLinksCol1 : any[] = [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}];
-  //navLinksCol2 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
-  //navLinksCol3 : any[] = [{ref:'', label:''}, {ref:'', label:''}, {ref:'', label:''}];
+  //navLinksCol2 : any[] = [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}];
+  //navLinksCol3 : any[] = [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}];
 }
