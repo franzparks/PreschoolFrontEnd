4dca547330d351512044bf2f294aaf10b4e93389
franzparks <franzparks@gmail.com>
2016-11-16 20:31:14 -0800

    fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,8 @@ import { SchoolsComponent } from './schools/schools.component';
     HttpModule,
 
   RouterModule.forRoot([
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: AppComponent },
+  //{ path: '', redirectTo: '/home', pathMatch: 'full' },
+  //{ path: 'home',  component: AppComponent },
   { path: 'schools',  component: SchoolsComponent },
   ]),
 
