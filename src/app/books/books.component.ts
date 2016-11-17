f5d82061512d0094291862e84091dc656e58ebc0
franzparks <franzparks@gmail.com>
2016-11-17 15:27:07 -0800

    import and inject book service

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from './book.service';
 
 @Component({
   selector: 'app-books',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bookService : BookService) { }
 
   ngOnInit() {
   }
