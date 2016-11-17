2dd8089d53ab4753e8876f1ea375e8b308714325
franzparks <franzparks@gmail.com>
2016-11-17 15:24:20 -0800

    import,provide school service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ActivitiesComponent } from './activities/activities.component';
 import { ToysComponent } from './toys/toys.component';
 import { SafetyComponent } from './safety/safety.component';
 import { BookService } from './book.service';
+import { SchoolService } from './school.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,6 @@ import { BookService } from './book.service';
     ActivitiesComponent,
     ToysComponent,
     SafetyComponent,
-    BookService
     
   ],
   imports: [
@@ -45,7 +45,7 @@ import { BookService } from './book.service';
   ]),
 
   ],
-  providers: [MenuService, BookService],
+  providers: [MenuService, BookService, SchoolService],
   bootstrap: [AppComponent]
 })
 
