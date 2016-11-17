f7b20ab8463272a8731f9aeb67e0be73409f9e7c
franzparks <franzparks@gmail.com>
2016-11-16 17:01:10 -0800

    add category data

--- src/app/nav-menu-items.ts
+++ src/app/nav-menu-items.ts
@@ -2,29 +2,65 @@ export const NAVITEMS : NavItem[] = [
 
 	{
 		id: 1,
-		navTitle : 'Main Title1',
-		navSubTitle  : 'Sub Title1',
-		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
-		navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
-        navLinksCol3 :[{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+		navTitle : 'Schools',
+		navSubTitle  : 'School Type',
+		navLinksCol1  : [{ref:'url', label:'Day Care'}, {ref:'url', label:'Preschool'}, {ref:'url', label:'Montesory'}],
+		navLinksCol2 : [],
+        navLinksCol3 :[]
 
 	},
 	{	
 		id: 2,
-		navTitle  : 'Main Title2',
-		navSubTitle  : 'Sub Title2',
-		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
-		navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+		navTitle  : 'Parenting',
+		navSubTitle  : 'Parenting Resources',
+		navLinksCol1  : [{ref:'url', label:'Parenting Books'}, {ref:'url', label:'Useful Sites'}],
+		navLinksCol2 : [],
+        navLinksCol3 : []
+
+	},
+	{
+		id: 3,
+		navTitle  : 'Childrens' Books',
+		navSubTitle  : 'Books For Children',
+		navLinksCol1  : [{ref:'url', label:'By Age'}, {ref:'url', label:'By Theme'}],
+		navLinksCol2 : [{ref:'url', label:'By Award'}],
         navLinksCol3 : [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
 
 	},
 	{
-		id: 6,
-		navTitle  : 'Main Title3',
-		navSubTitle  : 'Sub Title3',
-		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
-		navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
-        navLinksCol3 : [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+		id: 4,
+		navTitle  : 'Songs List',
+		navSubTitle  : 'Songs',
+		navLinksCol1  : [{ref:'url', label:'By Theme'}, {ref:'url', label:'By Scenario'}],
+		navLinksCol2 : [{ref:'url', label:'By Title'}],
+        navLinksCol3 : [{ref:'url', label:'By Artist'}]
 
-	}
+	},
+	{
+		id: 5,
+		navTitle  : 'Activity List',
+		navSubTitle  : 'Activities',
+		navLinksCol1  : [{ref:'url', label:'STEM'}, {ref:'url', label:'Art'}],
+		navLinksCol2 : [{ref:'url', label:'Social studies'}],
+        navLinksCol3 : []
+
+	},
+	{	
+		id: 6,
+		navTitle  : 'Toys List',
+		navSubTitle  : 'Recommended Toys',
+		navLinksCol1  : [{ref:'url', label:'By Age'}, {ref:'url', label:'By Development'}],
+		navLinksCol2 : [],
+        navLinksCol3 : []
+
+	},
+	{
+		id: 7,
+		navTitle  : 'Safety &amp; Health',
+		navSubTitle  : 'Safety &amp; Health',
+		navLinksCol1  : [{ref:'url', label:'Product Reviews'}, {ref:'url', label:'Nutrition Guide'}],
+		navLinksCol2 : [{ref:'url', label:'Recipes'}],
+        navLinksCol3 : []
+
+	},
 ];
