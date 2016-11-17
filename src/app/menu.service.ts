dfa66ba56a6bdde914410723e02a57690e3b8420
franzparks <franzparks@gmail.com>
2016-11-16 16:06:33 -0800

    add method to return nav items

--- src/app/menu.service.ts
+++ src/app/menu.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
 
+const NAVITEMS : NavItem[] = [
+
+	{
+		id: 1,
+		navTitle : 'Main Title1',
+		navSubTitle  : 'Sub Title1',
+		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		//navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+        //navLinksCol3 :[{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+
+	},
+	{	
+		id: 2,
+		navTitle  : 'Main Title2',
+		navSubTitle  : 'Sub Title2',
+		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+        navLinksCol3 : [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+
+	},
+	{
+		id: 6,
+		navTitle  : 'Main Title3',
+		navSubTitle  : 'Sub Title3',
+		navLinksCol1  : [{ref:'url', label:'Link1'}, {ref:'url', label:'Link2'}, {ref:'url', label:'Link3'}],
+		//navLinksCol2 : [{ref:'url', label:'Link4'}, {ref:'url', label:'Link5'}, {ref:'url', label:'Link5'}],
+        //navLinksCol3 : [{ref:'url', label:'Link7'}, {ref:'url', label:'Link8'}, {ref:'url', label:'Link9'}]
+
+	}
+];
+
 @Injectable()
 export class MenuService {
-
+  
+  navItems : NavItem[] = [];
   constructor() { }
+
+  getNavItems() : NavItem[] {
+  	return this.navItems;
+  }
 
 }
