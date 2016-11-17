a7612e366542fb6d5338a6d3f0737b8a8ed1be83
franzparks <franzparks@gmail.com>
2016-11-16 19:48:14 -0800

    update refs in nav-item data

--- src/app/nav-menu-items.ts
+++ src/app/nav-menu-items.ts
@@ -6,7 +6,7 @@ export const NAVITEMS : NavItem[] = [
 		id: 1,
 		navTitle : 'Schools',
 		navSubTitle  : 'School Type',
-		navLinksCol1  : [{ref:'url', label:'Day Care'}, {ref:'url', label:'Preschool'}, {ref:'url', label:'Montesory'}],
+		navLinksCol1  : [{ref:'schools', label:'Day Care'}, {ref:'schools', label:'Preschool'}, {ref:'schools', label:'Montesory'}],
 		navLinksCol2 : [],
         navLinksCol3 :[]
 
@@ -15,7 +15,7 @@ export const NAVITEMS : NavItem[] = [
 		id: 2,
 		navTitle  : 'Parenting',
 		navSubTitle  : 'Parenting Resources',
-		navLinksCol1  : [{ref:'url', label:'Parenting Books'}, {ref:'url', label:'Useful Sites'}],
+		navLinksCol1  : [{ref:'parenting', label:'Parenting Books'}, {ref:'parenting', label:'Useful Sites'}],
 		navLinksCol2 : [],
         navLinksCol3 : []
 
@@ -24,8 +24,8 @@ export const NAVITEMS : NavItem[] = [
 		id: 3,
 		navTitle  : 'Childrens\' Books',
 		navSubTitle  : 'Books For Children',
-		navLinksCol1  : [{ref:'url', label:'By Age'}, {ref:'url', label:'By Theme'}],
-		navLinksCol2 : [{ref:'url', label:'By Award'}],
+		navLinksCol1  : [{ref:'books', label:'By Age'}, {ref:'books', label:'By Theme'}],
+		navLinksCol2 : [{ref:'books', label:'By Award'}],
         navLinksCol3 : []
 
 	},
@@ -33,17 +33,17 @@ export const NAVITEMS : NavItem[] = [
 		id: 4,
 		navTitle  : 'Songs List',
 		navSubTitle  : 'Songs',
-		navLinksCol1  : [{ref:'url', label:'By Theme'}, {ref:'url', label:'By Scenario'}],
-		navLinksCol2 : [{ref:'url', label:'By Title'}],
-        navLinksCol3 : [{ref:'url', label:'By Artist'}]
+		navLinksCol1  : [{ref:'songs', label:'By Theme'}, {ref:'songs', label:'By Scenario'}],
+		navLinksCol2 : [{ref:'songs', label:'By Title'}],
+        navLinksCol3 : [{ref:'songs', label:'By Artist'}]
 
 	},
 	{
 		id: 5,
 		navTitle  : 'Activity List',
 		navSubTitle  : 'Activities',
-		navLinksCol1  : [{ref:'url', label:'STEM'}, {ref:'url', label:'Art'}],
-		navLinksCol2 : [{ref:'url', label:'Social studies'}],
+		navLinksCol1  : [{ref:'activities', label:'STEM'}, {ref:'activities', label:'Art'}],
+		navLinksCol2 : [{ref:'activities', label:'Social studies'}],
         navLinksCol3 : []
 
 	},
@@ -51,7 +51,7 @@ export const NAVITEMS : NavItem[] = [
 		id: 6,
 		navTitle  : 'Toys List',
 		navSubTitle  : 'Recommended Toys',
-		navLinksCol1  : [{ref:'url', label:'By Age'}, {ref:'url', label:'By Development'}],
+		navLinksCol1  : [{ref:'toys', label:'By Age'}, {ref:'toys', label:'By Development'}],
 		navLinksCol2 : [],
         navLinksCol3 : []
 
@@ -60,8 +60,8 @@ export const NAVITEMS : NavItem[] = [
 		id: 7,
 		navTitle  : 'Safety And Health',
 		navSubTitle  : 'Safety And Health',
-		navLinksCol1  : [{ref:'url', label:'Product Reviews'}, {ref:'url', label:'Nutrition Guide'}],
-		navLinksCol2 : [{ref:'url', label:'Recipes'}],
+		navLinksCol1  : [{ref:'safety', label:'Product Reviews'}, {ref:'safety', label:'Nutrition Guide'}],
+		navLinksCol2 : [{ref:'safety', label:'Recipes'}],
         navLinksCol3 : []
 
 	},
