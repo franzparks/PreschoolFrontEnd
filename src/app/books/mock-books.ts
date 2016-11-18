667fcee40fe9c7f4a34a05332fc54be0b5a1fadd
franzparks <franzparks@gmail.com>
2016-11-17 16:33:35 -0800

    add mock books

---
+++ src/app/books/mock-books.ts
@@ -0,0 +1,42 @@
+import { NavItem } from './nav-item';
+
+export const NAVITEMS : NavItem[] = [
+
+	{
+		id: 1,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+	},
+	{	
+		id: 2,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+	},
+	{
+		id: 3,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+	},
+	{
+		id: 4,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+
+	},
+	{
+		id: 5,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+
+	},
+	{	
+		id: 6,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+	},
+	{
+		id: 7,
+		name : 'Diary of a Wimpy Kid',
+		author  : 'Someone',
+	},
+];
