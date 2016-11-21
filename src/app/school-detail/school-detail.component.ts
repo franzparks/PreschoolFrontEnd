acafb03c30128e6999f1094ec8686b6215d74660
franzparks <franzparks@gmail.com>
2016-11-21 11:37:16 -0800

    import school service

--- src/app/school-detail/school-detail.component.ts
+++ src/app/school-detail/school-detail.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
+import { SchoolService } from '../school.service';
+
 import {School} from '../school';
 
 
