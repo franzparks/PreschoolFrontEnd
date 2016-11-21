cbe54ff4e6ff56d63acefb66a719a900a2010830
franzparks <franzparks@gmail.com>
2016-11-21 11:34:41 -0800

    import school

--- src/app/school-detail/school-detail.component.ts
+++ src/app/school-detail/school-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {School} from '../school';
 
 @Component({
   selector: 'app-school-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolDetailComponent implements OnInit {
 
-  constructor() { }
+	@Input() school: School;	
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+
+	}
 
 }
