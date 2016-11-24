d8592eb47670f009a318989f6f2ee9c850beaf42
franzparks <franzparks@gmail.com>
2016-11-24 10:05:07 -0800

    remove books css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MenuService } from './menu.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css',String('../styles.css')],
+  styleUrls: ['./app.component.css',String('../styles.css')], //
 
 })
 
