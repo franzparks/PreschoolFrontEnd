84562ef0f4efbe62c68e06ec6b7d29e2980d0ea5
franzparks <franzparks@gmail.com>
2016-11-28 19:54:49 -0800

    add  mock schools

---
+++ src/app/schools/mock-schools.ts
@@ -0,0 +1,18 @@
+export const SCHOOLS = [
+	{
+		id: 1,
+		name : 'Harker'
+	},
+	{
+		id: 2,
+		name : 'Challenger'
+	},
+	{
+		id: 3,
+		name : 'South Bay'
+	},
+	{
+		id: 4,
+		name : 'Montessori'
+	},
+];
