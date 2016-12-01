ec1ba724e037f858d6fb9e84c68c9cafdb9e48a6
franzparks <franzparks@gmail.com>
2016-12-01 15:23:10 -0800

    add error handler method

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -29,4 +29,10 @@ export class BookService {
 	  				.then(books => books.find(book => book.id === id));
 	}
 
+	private handleError(error: any): Promise<any> {
+  		console.error('An error occurred', error); // for demo purposes only
+  		return Promise.reject(error.message || error);
+	}
+
+
 }
