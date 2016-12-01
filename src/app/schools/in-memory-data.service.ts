1c5da74e0e47b7ba2673fe4fa5fdcb89ac184168
franzparks <franzparks@gmail.com>
2016-11-30 21:22:35 -0800

    add in memory Db

--- src/app/schools/in-memory-data.service.ts
+++ src/app/schools/in-memory-data.service.ts
@@ -1,1 +1,25 @@
 import {InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+	createDb() {
+		let schools = [
+			{
+				id: 1,
+				name : 'Harker'
+			},
+			{
+				id: 2,
+				name : 'Challenger'
+			},
+			{
+				id: 3,
+				name : 'South Bay'
+			},
+			{
+				id: 4,
+				name : 'Montessori'
+			},
+		];
+		return {schools};
+	}
+}
