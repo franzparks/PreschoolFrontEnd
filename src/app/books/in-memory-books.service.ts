97c5a3fa6660e8adb0be0b3b074cc68db3a00802
franzparks <franzparks@gmail.com>
2016-12-01 15:08:44 -0800

    add books to in memory book service

---
+++ src/app/books/in-memory-books.service.ts
@@ -0,0 +1,51 @@
+import {InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryBookDataService implements InMemoryDbService {
+	createDb() {
+		let books = [
+			{
+				id: 1,
+				name : 'Diary of a Wimpy Kid',
+				author  : 'Someone',
+			},
+			{	
+				id: 2,
+				name : 'Tale of Two Cities',
+				author  : 'Charles Dickens',
+			},
+			{
+				id: 3,
+				name : 'Oliver Twist',
+				author  : 'Charles Dickens',
+			},
+			{
+				id: 4,
+				name : 'The Buttefly',
+				author  : 'Boyi Sun',
+
+			},
+			{
+				id: 5,
+				name : 'Purple Roses',
+				author  : 'Clarissa Phiri',
+
+			},
+			{	
+				id: 6,
+				name : 'Adventures of Sherlock Holmes',
+				author  : 'Sir Conon Doyle',
+			},
+			{
+				id: 7,
+				name : 'Dancing in the Sun',
+				author  : 'Clarissa Phiri',
+			},
+			{
+				id: 8,
+				name : 'Sheep Among Wolves',
+				author  : 'Clarissa Phiri',
+			},
+	];
+		return {books};
+	}
+}
