f6e3a471a4f2abd3d2758629a037db530d088ca7
franzparks <franzparks@gmail.com>
2016-12-01 16:33:51 -0800

    move files to correct folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 import { RouterModule }   from '@angular/router';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './schools/in-memory-data.service';
+import { InMemoryDataService }  from './in-memory-data.service';
 //import { InMemoryBooksDataService }  from './books/in-memory-books.service';
 
 import { AppComponent } from './app.component';
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,6 +1,6 @@
 import {InMemoryDbService } from 'angular-in-memory-web-api';
-import {BOOKS } from '../books/mock-books'
-import {SCHOOLS} from './mock-schools';
+import {BOOKS } from './books/mock-books'
+import {SCHOOLS} from './schools/mock-schools';
 
 export class InMemoryDataService implements InMemoryDbService {
 	createDb() {
