cccc5c6709f2c7c1f2fcc4b4b908ce2f50bec7f6
franzparks <franzparks@gmail.com>
2016-12-03 22:31:01 -0800

    update class names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,8 @@ export class AppComponent {
   navStyleOneCol = 'dropdown-grid-container';
   navStyleTwoCol = 'dropdown-grid-container dropdown-grid-container-two-column';
   navStyleThreeCol = 'dropdown-grid-container dropdown-grid-container-three-column';
-  yes = 'nav2';
-  no = 'nav2 visibility';
+  navVisible = 'nav2';
+  navInvincible = 'nav2 invincible';
   navItems : NavItem[] = [];
   visible : boolean = false; 
   
--- src/app/schools/school.service.ts
+++ src/app/schools/school.service.ts
@@ -34,6 +34,6 @@ export class SchoolService {
 	private handleError(error: any): Promise<any> {
   		console.error('An error occurred', error); // for demo purposes only
   		return Promise.reject(error.message || error);
-}
+	}
 
 }
