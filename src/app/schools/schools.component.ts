a357a4ccf30eca55025c6aff63997a9b1931697f
franzparks <franzparks@gmail.com>
2017-01-22 18:21:46 -0800

    get books on init

--- src/app/schools/schools.component.ts
+++ src/app/schools/schools.component.ts
@@ -15,8 +15,15 @@ export class SchoolsComponent implements OnInit {
 	constructor(private schoolService : SchoolService) { }
 
 	ngOnInit() {
-		this.schoolService.getSchools().then(schools => this.schools = schools);
-		this.schoolService.getItems().then(items => this.items = items);
+		//this.schoolService.getSchools().then(schools => this.schools = schools);
+		//this.schoolService.getItems().then(items => this.items = items);
+		this.schoolService.getSchoolList().subscribe(
+			res => {
+        		this.schools = JSON.parse(JSON.parse(JSON.stringify(res))._body);
+        		//console.log(JSON.parse(JSON.stringify(res)));	
+      		},
+      		error => console.log(error)
+		)
 	}
 
 }
